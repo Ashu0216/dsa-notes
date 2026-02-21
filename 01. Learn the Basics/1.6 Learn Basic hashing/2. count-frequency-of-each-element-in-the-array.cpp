257d2daf3ac99182bdc863a87f65486ef08aec63
Ashu <ashuagarwal3017@gmail.com>
2026-02-21 19:25:39 +0530

    feat: learn basic hashing

---
+++ 01. Learn the Basics/1.6 Learn Basic hashing/2. count-frequency-of-each-element-in-the-array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Frequency(int arr[], int n) {
+
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < n; i++) {
+        map[arr[i]]++;
+    }
+
+    for (auto x : map) {
+        cout << x.first << " " << x.second << endl;
+    }
+}
